3b57dd5fd7567cddf2c3d8362ac94f28b9b1852a
Zacnhere <168651993+Zacnhere@users.noreply.github.com>
2025-03-27 13:55:43 +0700

    Update config.py

--- PremSub/config.py
+++ PremSub/config.py
@@ -8,7 +8,7 @@ load_dotenv(".env")
 
 
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7664621137:AAGKbBpdOBfApMxF4lCtgQHEjUpJhucZGls")
 API_ID = int(os.environ.get("API_ID", "18932594"))
 API_HASH = os.environ.get("API_HASH", "")
 MONGO_URL = os.environ.get("MONGO_URL", "")

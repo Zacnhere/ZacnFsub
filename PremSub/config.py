64c43e7300428662531df94fa8f35fc4a55226d6
Zacnhere <168651993+Zacnhere@users.noreply.github.com>
2025-03-27 14:03:07 +0700

    Update config.py

--- PremSub/config.py
+++ PremSub/config.py
@@ -9,13 +9,13 @@ load_dotenv(".env")
 
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "7664621137:AAGKbBpdOBfApMxF4lCtgQHEjUpJhucZGls")
-API_ID = int(os.environ.get("API_ID", "18932594"))
-API_HASH = os.environ.get("API_HASH", "")
+API_ID = int(os.environ.get("API_ID", "8986091"))
+API_HASH = os.environ.get("API_HASH", "c568be6936fb9df2a9ac17cce099e748")
 MONGO_URL = os.environ.get("MONGO_URL", "")
-ADMINS = [int(x) for x in (os.environ.get("ADMINS", "1325957770").split())]
-MEMBER = [int(x) for x in (os.environ.get("MEMBER", "1325957770").split())]
-LOG_GRP = int(os.environ.get("LOG_GRP", "-1002173200214"))
-BOT_ID = int(os.environ.get("BOT_ID", "7152159664"))
-NamaData = os.environ.get("NamaData", "ZeebFsub")
+ADMINS = [int(x) for x in (os.environ.get("ADMINS", "1361379181").split())]
+MEMBER = [int(x) for x in (os.environ.get("MEMBER", "").split())]
+LOG_GRP = int(os.environ.get("LOG_GRP", "-1002166782827"))
+BOT_ID = int(os.environ.get("BOT_ID", "7664621137"))
+NamaData = os.environ.get("NamaData", "ZacnFsub")
 
 KITA = [int(x) for x in (os.environ.get("KITA", "1325957770 1325957770").split())]
